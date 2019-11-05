f7b8050af97debd6385285e9a2bff30f833f27ee
Rafael Delboni <rafadelboni@gmail.com>
2019-11-05 18:49:17 -0300

    feat: Removes initial-state & move data to pathom
    Stopped at 3.13.2. Handling Mutations on The Server

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -2,12 +2,13 @@
   (:require
     [app.application :refer [app]]
     [app.ui :as ui]
-    [com.fulcrologic.fulcro.application :as app]))
+    [com.fulcrologic.fulcro.application :as app]
+    [com.fulcrologic.fulcro.data-fetch :as df]))
 
-(defn ^:export init
-  "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
-  []
+(defn ^:export init []
   (app/mount! app ui/Root "app")
+  (df/load! app :friends ui/PersonList)
+  (df/load! app :enemies ui/PersonList)
   (js/console.log "Loaded"))
 
 (defn ^:export refresh
